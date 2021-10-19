08ba1ec38307552852514afb65e2b3c2a9087fae
Taina <vanillaros@gmail.com>
2021-10-19 10:13:22 -0300

    estruturando request e test de PostAuth

---
+++ src/test/java/br/com/restassuredapitest/tests/auth/tests/PostAuthTest.java
@@ -0,0 +1,22 @@
+package br.com.restassuredapitest.tests.auth.tests;
+
+import br.com.restassuredapitest.suites.AllTests;
+import br.com.restassuredapitest.tests.auth.requests.PostAuthRequest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+public class PostAuthTest {
+
+    PostAuthRequest postAuthRequest = new PostAuthRequest();
+
+    @Test
+    @Category(AllTests.class)
+    public void validaRetornoDeTokenParaUsuario(){
+        postAuthRequest.tokenReturn()
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue());
+    }
+}

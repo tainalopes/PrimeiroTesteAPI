6f61a499e28b8c868daca8c4896660ebd4e63b95
Taina <vanillaros@gmail.com>
2021-10-20 10:03:37 -0300

    adicionado payload de Auth

---
+++ src/test/java/br/com/restassuredapitest/tests/auth/requests/payloads/AuthPayloads.java
@@ -0,0 +1,12 @@
+package br.com.restassuredapitest.tests.auth.requests.payloads;
+
+import org.json.JSONObject;
+
+public class AuthPayloads {
+    public JSONObject jsonAuthLogin(){
+        JSONObject payloadLogin = new JSONObject();
+        payloadLogin.put("username", "admin");
+        payloadLogin.put("password", "password123");
+        return payloadLogin;
+    }
+}

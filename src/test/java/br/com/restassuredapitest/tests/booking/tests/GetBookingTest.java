8d848633a1867513fdfb4eb05630dac1ec8a1a34
Taina <vanillaros@gmail.com>
2021-10-21 10:40:45 -0300

    primeiro teste de schema e criação do utils

--- src/test/java/br/com/restassuredapitest/tests/booking/tests/GetBookingTest.java
+++ src/test/java/br/com/restassuredapitest/tests/booking/tests/GetBookingTest.java
@@ -3,8 +3,11 @@ package br.com.restassuredapitest.tests.booking.tests;
 import br.com.restassuredapitest.base.BaseTest;
 import br.com.restassuredapitest.suites.AllTests;
 import br.com.restassuredapitest.tests.booking.requests.GetBookingRequest;
+import br.com.restassuredapitest.utils.Utils;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import java.io.File;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.greaterThan;
 
 public class GetBookingTest extends BaseTest {
@@ -18,5 +21,14 @@ public class GetBookingTest extends BaseTest {
                     .then()
                     .statusCode(200)
                     .body("size()", greaterThan(0));
+    }
+
+    @Test
+    @Category({})
+    public void validaSchemaDaListagemDeReservas(){
+        getBookingRequest.bookingReturnIds()
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchema(new File(Utils.getSchemaBasePath("booking", "bookings"))));
     }
 }
